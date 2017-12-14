a96209880ac37c8843bbaefbabbfa294d0fa8d83
StephenAWalsh <steve.walsh@avalara.com>
2017-12-14 11:26:35 -0800

    DO NOT MERGE - This is for testing campaign IDs only!

--- public/js/new-ava-form.js
+++ public/js/new-ava-form.js
@@ -1,5 +1,5 @@
 function insertAvaForm() {
-    $('.avaform-wrapper').append('<div style="margin: 20px 20px 20px 20px; padding: 20px 20px 20px 20px;"> <label>Request a Free AvaTax Sandbox Account</label> <form> <div class="form-group"> <label for="name">First Name / Given Name</label> <input type="text" class="form-control" id="firstName" placeholder=""> </div> <div class="form-group"> <label for="name">Last Name / Family Name</label> <input type="text" class="form-control" id="lastName" placeholder=""> </div> <div class="form-group"> <label for="email">Email Address</label> <input type="email" class="form-control" id="email" placeholder="user@example.org"> </div> <div class="form-group"> <label for="company">Company</label> <input type="text" class="form-control" id="company" placeholder="Company Name"> </div> <div class="form-group"> <label for="website">Website</label> <input type="text" class="form-control" id="website" placeholder="Optional"> </div> <div class="form-group"> <label for="phone">Phone</label> <input type="tel" class="form-control" id="phone" aria-describedby="emailHelp" placeholder="Phone Number"> </div> <div class="form-group" style="float:left;width:66%;margin-right:2%;"> <label for="street-address">Street Address</label> <input type="text" class="form-control" id="street-address" placeholder=""> </div> <div class="form-group" style="float:left;width:32%;"> <label for="city">City</label> <input type="text" class="form-control" id="city" placeholder=""> </div> <div class="form-group" style="float:left;width:32%;margin-right:2%;"> <label for="region">Region</label> <input type="text" class="form-control" id="region" placeholder="Region / State / Province"> </div> <div class="form-group" style="float:left;width:32%;margin-right:2%;"> <label for="country">Country</label> <input type="text" class="form-control" id="country" placeholder=""> </div> <div class="form-group" style="float:left;width:32%;"> <label for="postal-code">Postal Code</label> <input type="text" class="form-control" id="postal-code" placeholder=""> </div> <button type="button" id="submit" class="btn btn-primary" onclick="return submitShort();" style="clear:both;">Submit</button> </form> <br/> <div id="ava-form-alert" class="alert alert-warning fade in" style="display: none;"> <span id="ava-form-info"></span> </div> </div>');
+    $('.avaform-wrapper').append('<div style="margin: 20px 20px 20px 20px; padding: 20px 20px 20px 20px;"> <label>Request a Free AvaTax Sandbox Account</label> <form> <div class="form-group"> <label for="name">First Name / Given Name</label> <input type="text" class="form-control" id="firstName" placeholder=""> </div> <div class="form-group"> <label for="name">Last Name / Family Name</label> <input type="text" class="form-control" id="lastName" placeholder=""> </div> <div class="form-group"> <label for="email">Email Address</label> <input type="email" class="form-control" id="email" placeholder="user@example.org"> </div> <div class="form-group"> <label for="company">Company</label> <input type="text" class="form-control" id="company" placeholder="Company Name"> </div> <div class="form-group"> <label for="website">Website</label> <input type="text" class="form-control" id="website" placeholder="Optional"> </div> <div class="form-group"> <label for="phone">Phone</label> <input type="tel" class="form-control" id="phone" aria-describedby="emailHelp" placeholder="Phone Number"> </div> <div class="form-group" style="float:left;width:66%;margin-right:2%;"> <label for="street-address">Street Address</label> <input type="text" class="form-control" id="street-address" placeholder=""> </div> <div class="form-group" style="float:left;width:32%;"> <label for="city">City</label> <input type="text" class="form-control" id="city" placeholder=""> </div> <div class="form-group" style="float:left;width:32%;margin-right:2%;"> <label for="region">Region</label> <input type="text" class="form-control" id="region" placeholder="Region / State / Province"> </div> <div class="form-group" style="float:left;width:32%;margin-right:2%;"> <label for="country">Country</label> <input type="text" class="form-control" id="country" placeholder=""> </div> <div class="form-group" style="float:left;width:32%;"> <label for="postal-code">Postal Code</label> <input type="text" class="form-control" id="postal-code" placeholder=""> </div> <div class="form-group"> <label for="campaign-id">Campaign ID</label> <input type="text" class="form-control" id="campaign-id" placeholder=""> </div> <button type="button" id="submit" class="btn btn-primary" onclick="return submitShort();" style="clear:both;">Submit</button> </form> <br/> <div id="ava-form-alert" class="alert alert-warning fade in" style="display: none;"> <span id="ava-form-info"></span> </div> </div>');
 }
 
 function submitShort() {
@@ -18,7 +18,8 @@ function submitShort() {
         company: $('#company').val(),
         website: $('#website').val(),
         phone: $('#phone').val(),
-        campaign: c_id,
+        // campaign: c_id,
+        campaign: $('#campaign-id').val(),
         companyAddress: {
             line: $('#street-address').val(),
             city: $('#city').val(),
@@ -28,7 +29,7 @@ function submitShort() {
         }
     };
 
-    // console.log("payload = ", JSON.stringify(payload));
+    console.log("payload = ", payload);
     $.ajax({
         url: 'https://sandbox-rest.avatax.com/api/v2/accounts/freetrials/request',
         data: JSON.stringify(payload),
@@ -42,6 +43,7 @@ function submitShort() {
             $('#ava-form-alert').attr('class', 'alert alert-warning fade in');
             $('#submit').prop('disabled', false);
             $('#ava-form-alert').show();
+            console.log("error: ", err);
         },
         dataType: 'json',
         success: function(data) {
@@ -49,7 +51,7 @@ function submitShort() {
             $('#ava-form-alert').attr('class', 'alert alert-success fade in');
             $('#submit').prop('disabled', false);
             $('#ava-form-alert').show();
-            // console.log(data);
+            console.log("response: ", data);
         },
         type: 'POST'
     });
